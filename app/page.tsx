4059338f9e27f7765041e7d4a11c23c62f05b9a9
Cooper  Proctor <cooperproctor@dhcp-vl2041-9150.eduroam.cornell.edu>
2025-01-23 15:32:17 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,73 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+    return (
+      <div>
+        <section id="about">
+          <div className="container about-container">
+            <h2 className="section-title">About Me</h2>
+            <p className="about-line">
+              Computer Science and Neuroscience student at Cornell University
+            </p>
+            <p className="about-line">
+              Developing a deep learning approach to detect iRBD from gaited walking patterns
+            </p>
+            <p className="about-paragraph">
+              I&apos;m interested in the intersection of computer science, neuroscience, and bioethics 
+              in connection to issues surrounding neurodegenerative diseases and changes in brain function 
+              resulting from social media use and other new technologies.
+            </p>
+            <p className="about-paragraph">
+              In the future, I hope to develop a wearable device that helps people optimize and 
+              control their attention and focus.
+            </p>
+          </div>
+        </section>
+        <section id="projects">
+          <div className="container">
+            <h2 className="section-title">Projects</h2>
+            <div className="project">
+                <a
+                    href="VR Virtual Grasping Final Paper.pdf"
+                    target="_blank"
+                >
+                Physics-Based Free-Hand Virtual Grasping in VR
+                </a>
+            </div>
+            <div className="project">
+                <a
+                    href="https://medium.com/@matthew.krakower21/generating-recipe-titles-with-neural-networks-4c669970c7d5"
+                    target="_blank"
+                >
+                RNN and Transfer Learning Recipe Generator
+                </a>
+            </div>
+            <div className="project">
+                <a
+                    href="https://www.babylonjs-playground.com/#BUXYWO"
+                    target="_blank"
+                >
+                Snowflakes 3D Animation
+                </a>
+            </div>
+            
+            <div className="project">
+                <a
+                    href="Greed Philosophy Final Paper.pdf"
+                    target="_blank"
+                >
+                Final Project Paper on the Philosophy of Greed
+                </a>
+            </div>
+            <div className="project">
+                <a
+                    href="Social Media:AI Final Paper.pdf"
+                    target="_blank"
+                >
+                Final Project Paper on Social Media and AI Echochambers
+                </a>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+  
